d890a6fa370e5f74c9c48d2a70e015bf3e800f75
danbas <danbascou@gmail.com>
2025-09-13 18:24:04 -0300

    fix: Icon button path

--- app/components/SubmitButton.tsx
+++ app/components/SubmitButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import arrowIcon from "/assets/images/icon-arrow.svg";
 
 interface Props {
   onCalculate: () => void;
@@ -13,7 +14,7 @@ const SubmitButton: React.FC<Props> = ({ onCalculate }) => {
         className="absolute right-1/2 translate-x-1/2 sm:right-0 sm:translate-x-0 bg-violet-600 p-4 sm:p-5 rounded-full hover:bg-black focus:outline-none"
       >
         <img
-          src="/assets/images/icon-arrow.svg"
+          src={arrowIcon}
           alt="Calculate age"
           className="w-6 h-6 sm:w-auto sm:h-auto"
         />
